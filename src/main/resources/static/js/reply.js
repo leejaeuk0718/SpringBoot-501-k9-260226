5a2999fc1fc81bb5a5cf359ca05176074d022ae6
lsy3709 <lsy3709@naver.com>
2026-03-16 12:52:12 +0900

    4교시, 댓글 작업, 화면에서, 댓글 목록 화면 출력, 페이지네이션 작업중

--- src/main/resources/static/js/reply.js
+++ src/main/resources/static/js/reply.js
@@ -3,5 +3,34 @@ async function get1(bno) {
     // 게시글에 대한 , 댓글 목록 조회.
     const result = await axios.get(`/replies/list/${bno}`)
     // 데이터를 받아오는지 여부를 확인.
-    console.log(result)
+    // console.log(result)
+     return result.data
 }
+
+// 비동기 통신으로 댓글의 목록 내용을 받아오는 함수
+async function getList({bno,page,size,goLast}) {
+    const result = await axios.get(`/replies/list/${bno}`, {params: {page, size}})
+    // 여기 안에, 댓글의 목록인 dtoList 가 포함되어 있다.
+    return result.data
+}
+
+// 화면에 출력하기 위한 함수, : 그림을 그리는 함수
+function printReplies(page, size, goLast) {
+    // 임시 테스트, 각자 게시글 번호에서, 댓글이 있는 부모 게시글 번호 이용하기.
+    const bnoValue = typeof bno !== 'undefined' ? bno : 125;
+    getList({bno: bnoValue, page, size, goLast}).then(
+
+        data => {
+            // 먼저 댓글 목록을 그리기
+            // 이 함수는 read.html 내부에 존재함.
+            printList(data.dtoList)
+            // 댓글의 페이지네이션 그려주는 함수 호출
+            printPages(data)
+        }
+    )
+}
+
+// 위에서, 화면을 그려주는 함수 호출해서, 실제 그림 그리기.
+// 임의로 그리기.
+printReplies(1,10)
+
