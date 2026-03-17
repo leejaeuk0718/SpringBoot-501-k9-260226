3bde025a6cc9166080d911a5a83895ea61388888
lsy3709 <lsy3709@naver.com>
2026-03-17 12:38:04 +0900

    4교시, 이미지 첨부 작업 썸네일 변환 작업 확인

--- src/main/java/com/busanit501/springboot0226/controller/UpDownController.java
+++ src/main/java/com/busanit501/springboot0226/controller/UpDownController.java
@@ -5,10 +5,13 @@ import com.busanit501.springboot0226.dto.upload.UploadFileDTO;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 import lombok.extern.log4j.Log4j2;
+import net.coobird.thumbnailator.Thumbnailator;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.File;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.UUID;
@@ -45,9 +48,26 @@ public class UpDownController {
                  // 랜덤하게 생성된 uuid 를 덧붙여서, 원본 파일명과 같이 사용함.
                  Path savePath = Paths.get(uploadPath, uuid+"_"+originName);
                  log.info(" UpDownController 이미지 첨부 경로 확인(uuid이용) savePath : " + savePath.toString()  );
-                 // 실제 경로에, 이미지 파일 저장.
+
                  try {
+                     // 실제 경로에, 이미지 파일 저장.
                      file.transferTo(savePath);
+
+                     // 첨부된 파일이 , 이미지이면, 썸네일 도구 이용해서, 이미지 화질 축소 작업.
+                     // savePath :
+                     // c:\\upload\\springTest\\5b418a60-407e-406e-991e-db88d35ea426_크롬기준-로컬스토리지 저장소 확인 방법.PNG
+                     // image/png, image/jpeg, image/jpg , 이런 형식으로 서버에 전달이 됩니다.
+                     if(Files.probeContentType(savePath).startsWith("image")) {
+                         // 자바에서, 새로운 파일을 생성하는 도구,
+                         // 예시 : s_5b418a60-407e-406e-991e-db88d35ea426_크롬기준-로컬스토리지 저장소 확인 방법.PNG
+                         // 주의사항은 파일명에 앞에 s_ 있는지를 확인 잘해야함. 우리는 썸네일로 약속.
+                         // 빈 파일 생성,
+                         File thumbFile = new File(uploadPath, "s_" + uuid+"_"+originName);
+                         // 썸네일 도구 이용해서, 축소된 이미지 내용을,  thumbFile 쓰기 작업, (바이트 작업),
+                         // 도구 이용.
+                         Thumbnailator.createThumbnail(savePath.toFile(), thumbFile, 200,200);
+
+                     }
                  }catch (Exception e) {
                      e.printStackTrace();
                  }
