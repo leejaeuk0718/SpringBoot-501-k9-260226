a88dc0d33dd2bec29562fbc73a0aef0d209e4009
lsy3709 <lsy3709@naver.com>
2026-03-23 12:20:37 +0900

    4교시, 스프링 시큐리티 정적 자원은 시큐리티 기본 필터 설정에서 제외하기

--- src/main/java/com/busanit501/springboot0226/config/CustomSecurityConfig.java
+++ src/main/java/com/busanit501/springboot0226/config/CustomSecurityConfig.java
@@ -2,9 +2,11 @@ package com.busanit501.springboot0226.config;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,11 +14,23 @@ import org.springframework.security.web.SecurityFilterChain;
 @RequiredArgsConstructor
 public class CustomSecurityConfig {
 
+    // 순서1
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         log.info("CustomSecurityConfig : SecurityFilterChain, 스프링 시작시, 검사를 한다. ");
 
         return http.build();
+    }
+
+    // 순서2
+    // css, js, 등 정적 자원은 시큐리티 필터에서 제외하기
+    // 임포트 선택시 참고:  import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        log.info("시큐리티 동작 확인 ====webSecurityCustomizer======================");
+        return (web) ->
+                web.ignoring()
+                        .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
     }
 }
