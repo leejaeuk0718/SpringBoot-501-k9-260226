89f765e56dbc7a597893833b2c55aaa8c05a1b49
lsy3709 <lsy3709@naver.com>
2026-03-24 12:37:23 +0900

    4교시, 스프링 시큐리티에서 지원해주는 자동로그인 설정 및 확인

--- src/main/java/com/busanit501/springboot0226/config/CustomSecurityConfig.java
+++ src/main/java/com/busanit501/springboot0226/config/CustomSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.busanit501.springboot0226.config;
 
+import com.busanit501.springboot0226.security.CustomUserDetailsService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
@@ -13,6 +14,10 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
+
+import javax.sql.DataSource;
 
 @Configuration
 @Log4j2
@@ -22,6 +27,11 @@ import org.springframework.security.web.SecurityFilterChain;
 // 권한별 설정 추가
 @EnableMethodSecurity()
 public class CustomSecurityConfig {
+
+    // 자동 로그인 순서1, 디비 접근 도구
+    private final DataSource dataSource;
+    // 시큐리티에서 로그인 처리를 담당하는 도구-,로그인한 유저 처리를 담당하는 부서
+    private final CustomUserDetailsService customUserDetailsService;
 
     // 순서1
     // 여기 메서드에, 중요한 시큐리티 인증, 인가 설정을 모두 함.
@@ -59,10 +69,18 @@ public class CustomSecurityConfig {
                 // 일반 유저와 관리자 모두 글쓰기 화면에 접근 가능
                 .requestMatchers("/board/register").hasAnyRole("USER", "ADMIN")
                 //
-                .requestMatchers("/admin","/images","/board/modify").hasRole("ADMIN")
+                .requestMatchers("/admin","/board/modify").hasRole("ADMIN")
                 // 위의 접근 제어 목록 외의 , 다른 어떤 요청이라도 반드시 인증이 되어야 접근이 된다.
-                .anyRequest().authenticated()
-        ;
+                .anyRequest().authenticated();
+
+        // 순서5 , 자동로그인.
+        http.rememberMe(
+                httpSecurityRememberMeConfigurer
+                        -> httpSecurityRememberMeConfigurer.key("12345678")
+                        .tokenRepository(persistentTokenRepository()) // 밑에서, 토큰 설정 추가해야해서,
+                        .userDetailsService(customUserDetailsService)
+                        .tokenValiditySeconds(60*60*24*30) //30일
+        );
 
         return http.build();
     }
@@ -84,4 +102,14 @@ public class CustomSecurityConfig {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+    // 자동 로그인 순서3,
+    @Bean
+    public PersistentTokenRepository persistentTokenRepository() {
+        // 시큐리티에서 정의 해둔 구현체
+        JdbcTokenRepositoryImpl repo = new JdbcTokenRepositoryImpl();
+        repo.setDataSource(dataSource);
+        return repo;
+    }
+    // 자동 로그인 순서3,
 }
