242f7261f8bc4db41a2d1e1c2aa349aca02dc006
lsy3709 <lsy3709@naver.com>
2026-03-25 12:19:06 +0900

    4교시,멤버 엔티티 클래스, 롤 정의 및 Repository 생성, 단위테스트, 더미 회원 추가 테스트

---
+++ src/test/java/com/busanit501/springboot0226/repository/MemberRepositoryTests.java
@@ -0,0 +1,61 @@
+package com.busanit501.springboot0226.repository;
+
+import com.busanit501.springboot0226.domain.Member;
+import com.busanit501.springboot0226.domain.MemberRole;
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.annotation.Commit;
+
+import java.util.Optional;
+import java.util.stream.IntStream;
+
+@SpringBootTest
+@Log4j2
+public class MemberRepositoryTests {
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Test
+    public void insertMembers() {
+        IntStream.rangeClosed(1,100).forEach(i ->{
+            Member member = Member.builder()
+                    .mid("member"+i)
+                    .mpw(passwordEncoder.encode("1111"))
+                    .email("email"+i+"@test.com")
+                    .build();
+
+            // 권한 추가.
+            member.addRole(MemberRole.USER);
+            if (i >=95) {
+                member.addRole(MemberRole.ADMIN);
+            }
+            memberRepository.save(member);
+        });
+    }
+
+    @Test
+    public void testRead() {
+        Optional<Member> result = memberRepository.getWithRoles("member100");
+        Member member = result.orElseThrow();
+        log.info(member);
+        log.info(member.getRoleSet());
+
+        member.getRoleSet().forEach(role -> log.info(role.name()));
+    }
+
+    @Commit
+    @Test
+    public void testUpdate() {
+        String mid = "lsy3709@kakao.com";
+        String mpw = passwordEncoder.encode("123456");
+        memberRepository.updatePassword(mpw, mid);
+    }
+
+
+}
